53ea6b113913930e4dd1910e6be269488f27ff84
drmcastles <drmcastles@yandex.ru>
2024-11-23 22:27:11 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,74 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Main {
+
+    private static final String apiKey = "b5eed699-c094-41ea-b9d5-1fab59194402";
+    private static final String baseURL = "https://api.weather.yandex.ru/v2/forecast";
+    private static final HttpClient httpClient = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(30)).build();
+
+    public static void main(String[] args) {
+        // Получаем константы от пользователя
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите широту: ");
+        String lat = scanner.nextLine();
+
+        System.out.println("Введите долготу: ");
+        String lon = scanner.nextLine();
+
+        System.out.println("Введите количество дней для расчета средней температуры: ");
+        int limit = Integer.parseInt(scanner.nextLine());
+
+        try {
+            // Запрос к сервису Яндекс погоды
+            String response = makeApiRequest(lat, lon, limit);
+            System.out.println("Полный ответ от сервиса: ");
+            System.out.println(response);
+
+            JSONObject jsonResponse = new JSONObject(response);
+            int currentTemp = jsonResponse.getJSONObject("fact").getInt("temp");
+            System.out.println("Текущая температура: " + currentTemp + "°C");
+
+            // Расчет средней температуры
+            double avgTemp = avgTemperature(jsonResponse, limit);
+            String text = String.format("Средняя температура за %d дней: %f °C", limit, avgTemp);
+            System.out.println(text);
+        } catch (Exception e) {
+            System.err.println("Error making HTTP request: " + e.getMessage());
+        }
+    }
+
+    // Метод для получения всех данных
+    private static String makeApiRequest(String lat, String lon, int limit) throws Exception {
+        String url = String.format("%s?lat=%s&lon=%s&limit=%d", baseURL, lat, lon, limit);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .headers("X-Yandex-Weather-Key", apiKey)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
+
+    // Метод для расчета средней температуры
+    private static double avgTemperature(JSONObject jsonResponse, int limit) {
+        JSONArray forecasts = jsonResponse.getJSONArray("forecasts");
+        double sum = 0.0;
+        int count = Math.min(limit, forecasts.length());
+
+        for(int i = 0; i < count; ++i) {
+            JSONObject forecast = forecasts.getJSONObject(i);
+            int dayTemp = forecast.getJSONObject("parts").getJSONObject("day").getInt("temp_avg");
+            sum += (double)dayTemp;
+        }
+        return sum / (double)count;
+    }
+}
